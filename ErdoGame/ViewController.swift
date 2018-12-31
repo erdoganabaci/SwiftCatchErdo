cf356bc423e91fc1b0b167b8acbf24c65c24ce9d
Erdo <erdo@erdos-mac.local>
2018-12-31 04:13:41 -0800

    Initial Commit

---
+++ ErdoGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ErdoGame
+//
+//  Created by Erdo on 31.12.2018.
+//  Copyright © 2018 Erdo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
